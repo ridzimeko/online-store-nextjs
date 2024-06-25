992797df8655e95f5a73480ab4eaa10f1e6a15e0
Amro Alfien <ridzimeko@proton.me>
2024-06-25 16:29:55 +0700

    feat: Login with Google

--- src/components/views/auth/login/index.tsx
+++ src/components/views/auth/login/index.tsx
@@ -56,7 +56,17 @@ const LoginView = () => {
             {isLoading ? "Loading..." : "Login"}
           </button>
         </form>
+        <hr className={styles.login__form__divider} />
+        <div className={styles.login__form__other}>
+          <button
+            onClick={() => signIn("google", { callbackUrl, redirect: false })}
+            className={styles.login__form__other__button}
+          >
+            <i className="bx bxl-google"></i> Login With Google
+          </button>
+        </div>
       </div>
+
       <p className={styles.login__link}>
         Don{"'"}t have an account? Sign up here{" "}
         <Link href="/auth/register">here</Link>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { signIn } from "@/lib/service";
+import GoogleProvider from "next-auth/providers/google";
+import { loginWithGoogle, signIn } from "@/lib/service";
 import { compare } from "bcrypt";
 import NextAuth from "next-auth/next";
 
@@ -32,6 +33,10 @@ const authOptions: NextAuthOptions = {
         }
       },
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_OAUTH_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET || "",
+    }),
   ],
   callbacks: {
     async jwt({ token, account, profile, user }: any) {
@@ -39,7 +44,21 @@ const authOptions: NextAuthOptions = {
         token.email = user.email;
         token.fullname = user.fullname;
         token.phone = user.phone;
-        token.role = user.rol;
+        token.role = user.role;
+      }
+
+      if (account?.provider === "google") {
+        const data = {
+          fullname: user.name,
+          email: user.email,
+          type: "google",
+        };
+
+        await loginWithGoogle(data, (data: any) => {
+          (token.email = data.email),
+            (token.fullname = data.fullname),
+            (token.role = data.role);
+        });
       }
 
       return token;
