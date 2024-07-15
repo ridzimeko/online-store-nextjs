3a4fd3243130121c97025483654297f5f487fe98
Amro Alfien <ridzimeko@proton.me>
2024-07-15 21:50:01 +0700

    feat: product management

---
+++ src/components/views/admin/Products/index.tsx
@@ -0,0 +1,105 @@
+import AdminLayout from "@/components/layouts/AdminLayout";
+import Button from "@/components/ui/Button";
+import styles from "./Product.module.scss";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import Image from "next/image";
+import { convertIDR } from "@/utils/currency";
+import { Product } from "@/types/product.type";
+
+type PropTypes = {
+  products: Product[];
+  setToaster: Dispatch<SetStateAction<{}>>;
+};
+
+const ProductsAdminView = (props: PropTypes) => {
+  const { products, setToaster } = props;
+  const [productsData, setProductsData] = useState<Product[]>([]);
+
+  console.log(products);
+
+  useEffect(() => {
+    setProductsData(products);
+  }, [products]);
+
+  return (
+    <>
+      <AdminLayout>
+        <div className={styles.products}>
+          <div>Product Management</div>
+          <table className={styles.products__table}>
+            <thead>
+              <tr>
+                <th rowSpan={2}>#</th>
+                <th rowSpan={2}>Image</th>
+                <th rowSpan={2}>Name</th>
+                <th rowSpan={2}>Category</th>
+                <th rowSpan={2}>Price</th>
+                <th colSpan={2}>Stock</th>
+                <th rowSpan={2}>Action</th>
+              </tr>
+              <tr>
+                <th>Size</th>
+                <th>Qty</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productsData.map((product, index) => (
+                <>
+                  <tr key={product.id}>
+                    <td rowSpan={product.stock.length}>{index + 1}</td>
+                    <td rowSpan={product.stock.length}>
+                      <Image
+                        src={product.image}
+                        alt={product.name}
+                        width={100}
+                        height={100}
+                        className={styles.products__table__image}
+                      ></Image>
+                    </td>
+                    <td rowSpan={product.stock.length}>{product.name}</td>
+                    <td rowSpan={product.stock.length}>{product.category}</td>
+                    <td rowSpan={product.stock.length}>
+                      {convertIDR(product.price)}
+                    </td>
+                    <td>{product.stock[0].size}</td>
+                    <td>{product.stock[0].qty}</td>
+                    <td rowSpan={product.stock.length}>
+                      <div className={styles.products__table__action}>
+                        <Button
+                          type="button"
+                          className={styles.products__table__action__edit}
+                        >
+                          <i className="bx bxs-edit"></i>
+                        </Button>
+                        <Button
+                          type="button"
+                          className={styles.products__table__action__delete}
+                        >
+                          <i className="bx bxs-trash-alt"></i>
+                        </Button>
+                      </div>
+                    </td>
+                  </tr>
+                  {product.stock.map(
+                    (stock: { size: string; qty: number }, index: number) => (
+                      <>
+                        {index > 0 && (
+                          <tr key={stock.size}>
+                            <td>{stock.size}</td>
+                            <td>{stock.qty}</td>
+                          </tr>
+                        )}
+                      </>
+                    )
+                  )}
+                </>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </AdminLayout>
+    </>
+  );
+};
+
+export default ProductsAdminView;
--- src/pages/admin/products/index.tsx
+++ src/pages/admin/products/index.tsx
@@ -1,11 +1,24 @@
-import AdminLayout from "@/components/layouts/AdminLayout";
+import ProductsAdminView from "@/components/views/admin/Products";
+import productServices from "@/services/product";
+import { useEffect, useState } from "react";
 
-const AdminProductpage = () => {
+const AdminProductsPage = ({ setToaster }: any) => {
+  const [products, setProducts] = useState([]);
+
+  const getAllProducts = async () => {
+    const { data } = await productServices.getAllProducts();
+    setProducts(data.data);
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
-    <AdminLayout>
-      <div>Admin Page</div>
-    </AdminLayout>
+    <>
+      <ProductsAdminView products={products} setToaster={setToaster} />
+    </>
   );
 };
 
-export default AdminProductpage;
+export default AdminProductsPage;
